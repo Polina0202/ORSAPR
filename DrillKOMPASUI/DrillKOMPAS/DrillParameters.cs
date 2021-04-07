a5f515406e67788f9e2281f5ae0315eae481b399
Полина Пилипенко <pol.pilipenko@yandex.ru>
2021-04-07 19:04:26 +0700

    Добовлен класс DrillParametrs

---
+++ DrillKOMPASUI/DrillKOMPAS/DrillParameters.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillKOMPAS
+{
+    /// <summary>
+    /// Класс с параметрами
+    /// </summary>
+    public class DrillParameters
+    {
+        /// <summary>
+        /// Длина дрели
+        /// </summary>
+        private double _drillLenght;
+
+        /// <summary>
+        /// Длина рабочей части
+        /// </summary>
+        private double _workingPartLenght;
+
+        /// <summary>
+        /// Диаметр дрели
+        /// </summary>
+        private double _drillDiameter;
+
+        /// <summary>
+        /// Длина лапки
+        /// </summary>
+        private double _tenonLenght;
+
+        /// <summary>
+        /// Ширина лапки
+        /// </summary>
+        private double _tenonWidth;
+
+        /// <summary>
+        /// Длина шейки
+        /// </summary>
+        private double _neckLenght;
+
+        /// <summary>
+        /// Ширина шейки
+        /// </summary>
+        private double _neckWidth;
+
+        /// <summary>
+        /// Свойство параметра: длина дрели
+        /// Диапазон параметра ограничен [3 .. 145] мм
+        /// </summary>
+        public double DrillLenght
+        {
+            get
+            {
+                return _drillLenght;
+            }
+            set
+            {
+                if (value < 3 || value > 145)
+                {
+                    throw new ArgumentException("Длина дрели не может быть меньше 3 мм и больше 145 мм");
+                }
+                else if (value < WorkingPartLenght)
+                {
+                    throw new ArgumentException("Длина сверла не может быть меньше длины рабочей части");
+                }
+                else
+                {
+                    _drillLenght = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Свойство параметра: длина рабочей части
+        /// Диапазон параметра ограничен [10 .. 30] мм
+        /// </summary>
+        public double WorkingPartLenght
+        {
+            get
+            {
+                return _workingPartLenght;
+            }
+            set
+            {
+                if (value < 10 || value > 30)
+                {
+                    throw new ArgumentException("Длина рабочей части не может быть меньше 10 мм и больше 30 мм");
+                }
+                else if (value > DrillLenght)
+                {
+                    throw new ArgumentException("Длина рабочей части не может быть больше длины сверла");
+                }
+                else
+                {
+                    _workingPartLenght = value;
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Свойство параметра: диаметр дрели
+        /// Диапазон параметра ограничен [0,25 .. 22] мм
+        /// </summary>
+        public double DrillDiameter
+        {
+            get
+            {
+                return _drillDiameter;
+            }
+            set
+            {
+                if (value < 3 || value > 145)
+                {
+                    throw new ArgumentException("Диаметр дрели не может быть меньше 0,25 мм и больше 22 мм");
+                }
+                else if (value < NeckWidth)
+                {
+                    throw new ArgumentException("Диаметр дрели не может быть меньше ширины шейки");
+                }
+                else if (value < TenonWidth)
+                {
+                    throw new ArgumentException("Диаметр дрели не может быть меньше ширины лапки");
+                }
+                else
+                {
+                    _drillDiameter = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Свойство параметра: длина лапки
+        /// Диапазон параметра ограничен [0 .. 22] мм
+        /// </summary>
+        public double TenonLenght
+        {
+            get
+            {
+                return _tenonLenght;
+            }
+            set
+            {
+                if (value < 0 || value > 22)
+                {
+                    throw new ArgumentException("Длина лапки не может быть меньше 0 мм и больше 22 мм");
+                }
+                else
+                {
+                    _tenonLenght = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Свойство параметра: Ширина лапки
+        /// Диапазон параметра ограничен [0 .. 22] мм
+        /// </summary>
+        public double TenonWidth
+        {
+            get
+            {
+                return _tenonWidth;
+            }
+            set
+            {
+                if (value < 0 || value > 22)
+                {
+                    throw new ArgumentException("Ширина лапки не может быть меньше 0 мм и больше 22 мм");
+                }
+                else if (value > DrillDiameter)
+                {
+                    throw new ArgumentException("Ширина лапки не может быть больше диаметра сверла");
+                }
+                else
+                {
+                    _tenonWidth = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Свойство параметра: длина шейки
+        /// Диапазон параметра ограничен [0 .. 14] мм
+        /// </summary>
+        public double NeckLenght
+        {
+            get
+            {
+                return _neckLenght;
+            }
+            set
+            {
+                if (value < 0 || value > 14)
+                {
+                    throw new ArgumentException("Длина шейки не может быть меньше 0 мм и больше 14 мм");
+                }
+                else if (DrillLenght - (WorkingPartLenght + TenonLenght + value) < 5)
+                {
+                    throw new ArgumentException("Не соблюдено условие: L - (a+d+l)>5");
+                }
+                else
+                {
+                    _neckLenght = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Свойство параметра: ширина шейки
+        /// Диапазон параметра ограничен [0 .. 10] мм
+        /// </summary>
+        public double NeckWidth
+        {
+            get
+            {
+                return _neckWidth;
+            }
+            set
+            {
+                if (value < 0 || value > 10)
+                {
+                    throw new ArgumentException("Ширина шейки не может быть меньше 0 мм и больше 10 мм");
+                }
+                else if (value > DrillDiameter)
+                {
+                    throw new ArgumentException("Ширина шейки не может быть больше диаметра сверла");
+                }
+                else
+                {
+                    _neckWidth = value;
+                }
+            }
+        }
+    }
+}
---
+++ DrillKOMPASUI/DrillKOMPASUI/MainForm.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using KompasWrapper;
+using DrillKOMPAS;
+
+namespace DrillKOMPASUI
+{
+    public partial class MainForm : Form
+    {
+        private Class1 _kompsWrapper = new Class1();
+
+        private DrillParameters _modelParameters;
+
+        private bool drillLenghtFlag;
+        private bool workingPartLenghtFlag;
+        private bool drillDiameterFlag;
+        private bool tenonLenghtFlag;
+        private bool tenonWidthFlag;
+        private bool neckLenghtFlag;
+        private bool neckWidthFlag;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _modelParameters = new DrillParameters();
+            
+            drillLenghtFlag = false;
+            workingPartLenghtFlag = false;
+            drillDiameterFlag = false;
+            tenonLenghtFlag = false;
+            tenonWidthFlag = false;
+            neckLenghtFlag = false;
+            neckWidthFlag = false;
+        }
+
+        /// <summary>
+        /// Проверка правильности заполнения полей
+        /// </summary>
+        /// <param name="maskedTextBox">поле параметра</param>
+        /// <param name="min">минимальная граница</param>
+        /// <param name="max">максимальная граница</param>
+        /// <param name="flagMaskedTextBox">правильность заполнения</param>
+        /// <param name="count">количество знаков в маске</param>
+        private bool ValidateCorrectInput(Control maskedTextBox, double min, double max, bool flagMaskedTextBox, int count)
+        {
+            if (maskedTextBox.Text.Length != count)
+            {
+                maskedTextBox.BackColor = Color.LightCoral;
+                return flagMaskedTextBox = false;
+            }
+            else
+            {
+                if (Convert.ToDouble(maskedTextBox.Text) < min 
+                    || Convert.ToDouble(maskedTextBox.Text) > max)
+                {
+                    maskedTextBox.BackColor = Color.LightCoral;
+                    toolTip.Show("Значение находится вне диапазона", maskedTextBox, 43, 17);
+                    return flagMaskedTextBox = false;
+                }
+                else
+                {
+                    maskedTextBox.BackColor = Color.White;
+                    return flagMaskedTextBox = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Функция, активирующая кнопку "Построения"
+        /// </summary>
+        private void ActivateButton()
+        {
+            if (drillLenghtFlag == true && workingPartLenghtFlag == true
+                                           && drillDiameterFlag == true && tenonLenghtFlag == true
+                                           && tenonWidthFlag == true && neckLenghtFlag == true
+                                           && neckWidthFlag == true)
+            {
+                buttonBuild.Enabled = true;
+            }
+            else
+            {
+                buttonBuild.Enabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Функция, предающая параметры с формы в проект
+        /// </summary>
+        private void ValidateModelParameters()
+        {
+            _modelParameters.DrillLenght = Convert.ToDouble(drillLenght.Text);
+            _modelParameters.WorkingPartLenght = Convert.ToDouble(workingPartLenght.Text);
+            _modelParameters.DrillDiameter = Convert.ToDouble(drillDiameter.Text);
+            _modelParameters.TenonLenght = Convert.ToDouble(tenonLenght.Text);
+            _modelParameters.TenonWidth = Convert.ToDouble(tenonWidth.Text);
+            _modelParameters.NeckLenght = Convert.ToDouble(neckLenght.Text);
+            _modelParameters.NeckWidth = Convert.ToDouble(neckWidth.Text);
+        }
+
+        //Нажатие кнопки
+        private void buttonBuild_Click(object sender, EventArgs e)
+        {
+            int amountErrors = 0;
+            int amountDependencies = 4;
+            string [] errorMessage = new string[amountDependencies];
+
+            if (Convert.ToDouble(drillDiameter.Text) <= Convert.ToDouble(neckLenght.Text))
+            {
+                errorMessage[amountErrors] = "Диаметр сверла (D) > Ширина шейки (c)";
+                amountErrors++;
+            }
+
+            if (Convert.ToDouble(drillDiameter.Text) <= Convert.ToDouble(tenonWidth.Text))
+            {
+                errorMessage[amountErrors] = "Диаметр сверла (D) > Ширина лапки (b)";
+                amountErrors++;
+            }
+
+            if (Convert.ToDouble(drillLenght.Text) <= Convert.ToDouble(workingPartLenght.Text))
+            {
+                errorMessage[amountErrors] = "Длина сверла (L) > Длина рабочей части (l)";
+                amountErrors++;
+            }
+
+            if (Convert.ToDouble(drillLenght.Text) - (Convert.ToDouble(workingPartLenght.Text) +
+                                                      Convert.ToDouble(tenonLenght.Text) +
+                                                      Convert.ToDouble(neckWidth.Text)) < 5)
+            {
+                errorMessage[amountErrors] = "Выражение неверно: L – (l + a + d) > 5";
+                amountErrors++;
+            }
+
+            if (amountErrors != 0)
+            {
+                MessageBox.Show(String.Join("\n", errorMessage), "Не соблюдены следующие условия:", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else
+            {
+                ValidateModelParameters();
+                //_kompsWrapper.Main();
+            }
+        }
+
+        // Проверка полей на правильное заполненение
+        private void drillLenght_TextChanged(object sender, EventArgs e)
+        {
+            drillLenghtFlag = ValidateCorrectInput( drillLenght, 3, 145, drillLenghtFlag, 6);
+            ActivateButton();
+        }
+
+        private void workingPartLenght_TextChanged(object sender, EventArgs e)
+        {
+            workingPartLenghtFlag = ValidateCorrectInput(workingPartLenght, 10, 30, workingPartLenghtFlag, 5);
+            ActivateButton();
+        }
+
+        private void drillDiameter_TextChanged(object sender, EventArgs e)
+        {
+            drillDiameterFlag = ValidateCorrectInput(drillDiameter, 0.25, 22, drillDiameterFlag, 5);
+            ActivateButton();
+        }
+
+        private void tenonLenght_TextChanged(object sender, EventArgs e)
+        {
+            tenonLenghtFlag = ValidateCorrectInput(tenonLenght, 0, 22, tenonLenghtFlag, 5);
+            ActivateButton();
+        }
+
+        private void tenonWidth_TextChanged(object sender, EventArgs e)
+        {
+            tenonWidthFlag = ValidateCorrectInput(tenonWidth, 0, 22, tenonWidthFlag, 5);
+            ActivateButton();
+        }
+
+        private void neckLenght_TextChanged(object sender, EventArgs e)
+        {
+            neckLenghtFlag = ValidateCorrectInput(neckLenght, 0, 14, neckLenghtFlag, 5);
+            ActivateButton();
+        }
+
+        private void neckWidth_TextChanged(object sender, EventArgs e)
+        {
+            neckWidthFlag = ValidateCorrectInput(neckWidth, 0, 10, neckWidthFlag, 5);
+            ActivateButton();
+        }
+    }
+}
