700f3bd74648dbe6ebd1ca43794791f8b8b256d7
Полина Пилипенко <pol.pilipenko@yandex.ru>
2021-04-07 19:37:22 +0700

    Создан класс юнит-тестов

---
+++ DrillKOMPASUI/Testing/DrillParameters.UnitTest/DrillParametersTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using Newtonsoft.Json;
+
+namespace DrillParameters.UnitTest
+{
+    [TestFixture]
+    class DrillParametersTest
+    {
+    }
+}
---
+++ DrillKOMPASUI/Testing/DrillParameters.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DrillParameters.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DrillParameters.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c582f18b-150b-4b9a-9c2b-f26a51e43158")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
